81f5369dc94eba84a5707d9be64ff54629482025
sanat-kapur <sanatkapur@gmail.com>
2018-10-27 21:56:59 -0400

    Update plot_function.R

--- R/plot_function.R
+++ R/plot_function.R
@@ -1,13 +1,13 @@
 pca_plot = function(metadata_file, gene_data_file) {
   #read in metadata
-  metadata = tryCatch(read.csv(metadata_filepath, colClasses = "character",
+  metadata = tryCatch(read.csv(metadata_file, colClasses = "character",
                       stringsAsFactors = F),
   error=function(e) {
     print("There was an error reading in the metadata CSV file. Please check error below.")
   })
   
   #read in gene data
-  gene_data = tryCatch(read.csv(gene_data_filepath, colClasses = "character",
+  gene_data = tryCatch(read.csv(gene_data_file, colClasses = "character",
                                 stringsAsFactors = F),
                        error=function(e) {
                          print("There was an error reading in the Gene Data CSV file. Please check error below.")
