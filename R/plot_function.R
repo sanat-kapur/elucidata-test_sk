59881292298f939260185c3854f654face8fa2b8
sanat-kapur <sanatkapur@gmail.com>
2018-10-27 20:57:25 -0400

    Add files via upload

---
+++ R/plot_function.R
@@ -0,0 +1,57 @@
+library(ggplot2)
+
+#metadata_filepath = "/Users/sanatkapur/OneDrive/Documents/ProgrammingProjects/elucidata_assignment/Assignment-2 Meta data sheet (2) (1) (1) (1).csv"
+
+#gene_data_filepath = "/Users/sanatkapur/OneDrive/Documents/ProgrammingProjects/elucidata_assignment/Assignment-2_gene_data- (2) (1) (1).csv"
+
+pca_plot = function(metadata_file, gene_data_file) {
+  #read in metadata
+  metadata = tryCatch(read.csv(metadata_filepath, colClasses = "character",
+                      stringsAsFactors = F),
+  error=function(e) {
+    print("There was an error reading in the metadata CSV file. Please check error below.")
+  })
+  
+  #read in gene data
+  gene_data = tryCatch(read.csv(gene_data_filepath, colClasses = "character",
+                                stringsAsFactors = F),
+                       error=function(e) {
+                         print("There was an error reading in the Gene Data CSV file. Please check error below.")
+                       })
+  
+  #convert all data columns to numeric
+  for (i in 3:32) {
+    gene_data[,i] = as.numeric(gene_data[,i])
+  }
+  
+  #remove all rows with NAs
+  gene_data = gene_data[complete.cases(gene_data),]
+  
+  #generate pca
+  gene_data_pca = prcomp(gene_data[,3:32],
+                         center = T,
+                         scale. = T)
+  
+  #data frame with PCA values 
+  gene_data_pca_vales = as.data.frame(gene_data_pca$x)
+  
+  theme<-theme(panel.background = element_blank(),panel.border=element_rect(fill=NA),panel.grid.major = element_blank(),panel.grid.minor = element_blank(),strip.background=element_blank(),axis.text.x=element_text(colour="black"),axis.text.y=element_text(colour="black"),axis.ticks=element_line(colour="black"),plot.margin=unit(c(1,1,1,1),"line"))
+  
+  plot = ggplot(gene_data_pca_vales,aes(x=PC1,y=PC2 )) + geom_point() + theme
+  
+  return(plot)
+}
+
+pca_plot(metadata_filepath, gene_data_filepath)
+
+
+
+
+
+
+
+
+
+
+
+
